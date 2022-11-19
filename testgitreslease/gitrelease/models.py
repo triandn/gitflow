fb10fdfd05ecd28fc2cdc778f929bf7fd8dbf09e
triandn <triandn43@gmail.com>
2022-11-19 09:53:57 +0700

    modified file models.py

--- testgitreslease/gitrelease/models.py
+++ testgitreslease/gitrelease/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=256 , default = '', blank = True)
     password = models.CharField(max_length=256 , default = '', blank = True)
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=256 , default = '', blank = True)
     
